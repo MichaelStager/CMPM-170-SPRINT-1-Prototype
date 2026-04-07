b84eec1c2300f1c3f92d1b44296347cb2f1978bb
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-04-07 11:56:48 -0700

    Color response to cam

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 using TMPro;
 using Unity.Cinemachine;
+using UnityEngine.UI;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] CinemachineCamera[] cameras;
+    [SerializeField] GameObject[] cameraButtons;
     private CinemachineCamera currentCamera;
+    private GameObject currentButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //CurrentCamera is set to the first camera in the array.
     void Start()
     {
         currentCamera = cameras[0];
+        currentButton = cameraButtons[0];
+        currentButton.GetComponent<Image>().color = Color.green;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //This function is called to switch the current camera to the camera at the specified index, it also changes the color of the button to green to give feedback on where you are. This is called through the onlcick() function of the buttons in the UI.
    public void SwitchCamera(int index)
     {
         if (index < 0 || index >= cameras.Length)
@@ -28,8 +29,11 @@ public class CameraManager : MonoBehaviour
         if (currentCamera != null)
         {
             currentCamera.gameObject.SetActive(false);
+            currentButton.GetComponent<Image>().color = Color.white; // Reset the color of the previous button to white
         }
         currentCamera = cameras[index];
         currentCamera.gameObject.SetActive(true);
+        currentButton = cameraButtons[index];
+        currentButton.GetComponent<Image>().color = Color.green; // Set the color of the current button to green to indicate it's active
     }
 }
