bbac3d17fd7108f7b453e3cc065074efd1bcc8c0
UtkuEfeAyan <76896911+UtkuEfeAyan@users.noreply.github.com>
2026-04-12 21:23:57 -0700

    Update KeyPad.cs

--- Assets/Scripts/KeyPad.cs
+++ Assets/Scripts/KeyPad.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class KeyPad : MonoBehaviour
 {
     public string correctCode = "1234567890"; // The correct code to unlock
+    private string enteredCode = ""; // The code entered by the player
+
+    private void Start()
+    {
+        // Optionally, you can initialize the enteredCode or set up any necessary references here
+        enteredCode = "";
+    }
     public void KeyPadButtonClicked(string buttonNumber)
     {
-        Debug.Log("Keypad button " + buttonNumber + " was clicked!");
-        // Add your logic here to handle the button click, such as checking if the correct code is entered.
+        //Debug.Log("Keypad button " + buttonNumber + " was clicked!");
+        enteredCode += buttonNumber;
+        Debug.Log ("Current entered code: " + enteredCode);
     }
 }
